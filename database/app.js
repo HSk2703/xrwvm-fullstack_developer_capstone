cc13df1b2beec6aacf58a2c9727f76fd70ca165d
HSk2703 <hs.ira0503@gmail.com>
2024-11-01 17:06:44 -0400

    .js files errors fixed for smooth linting

--- database/app.js
+++ database/app.js
@@ -1,11 +1,12 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -20,10 +21,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
@@ -33,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -69,14 +70,15 @@ try {
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here - Added the code below
-try {
-    const stateDealers = await Dealerships.find({ state: req.params.state });
-    res.json(stateDealers);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching dealerships by state' });
-  }
+    // Write your code here - Added the code below
+    try {
+        const stateDealers = await Dealerships.find({ state: req.params.state });
+        res.json(stateDealers);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships by state' });
+    }
 });
+
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
@@ -101,20 +103,21 @@ try {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0]['id']+1;
 
-  const review = new Reviews({
-		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
-	});
+const review = new Reviews({
+    "id": new_id,
+    "name": data.name, // Changed from data['name'] to data.name
+    "dealership": data.dealership, // Changed from data['dealership'] to data.dealership
+    "review": data.review, // Changed from data['review'] to data.review
+    "purchase": data.purchase, // Changed from data['purchase'] to data.purchase
+    "purchase_date": data.purchase_date, // Changed from data['purchase_date'] to data.purchase_date
+    "car_make": data.car_make, // Changed from data['car_make'] to data.car_make
+    "car_model": data.car_model, // Changed from data['car_model'] to data.car_model
+    "car_year": data.car_year, // Changed from data['car_year'] to data.car_year
+});
+
 
   try {
     const savedReview = await review.save();
